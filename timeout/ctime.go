816ae89dc07c165710b6f26496c82b27ea1261ad
songzhibin97 <718428482@qq.com>
2020-12-28 12:18:38 +0800

    超时控制增加

---
+++ timeout/ctime.go
@@ -0,0 +1,52 @@
+package ctime
+
+import (
+	"context"
+	"database/sql/driver"
+	"strconv"
+	"time"
+)
+
+// ctime GKIT时间模块
+// 主要提供context超时控制
+
+// Shrink: 用于链路超时时间以及当前节点的超时时间控制
+func Shrink(c context.Context, d time.Duration) (time.Duration, context.Context, context.CancelFunc) {
+	if deadline, ok := c.Deadline(); ok {
+		if timeout := time.Until(deadline); timeout < d {
+			// 链路超时时间已经小于当前节点的超时时间了,所以以上流链路为准,不重新设置
+			return timeout, c, func() {}
+		}
+	}
+	// 说明没有设置timeout或者deadline
+	ctx, cancel := context.WithTimeout(c, d)
+	return d, ctx, cancel
+}
+
+// Time 用于MySQL时间戳转换
+// 实现了 sql.Scanner 接口
+type Time int64
+
+// Scan 扫描赋值
+func (jt *Time) Scan(src interface{}) (err error) {
+	// 断言,只处理string以及原生的time.Time
+	switch sc := src.(type) {
+	case time.Time:
+		*jt = Time(sc.Unix())
+	case string:
+		var i int64
+		i, err = strconv.ParseInt(sc, 10, 64)
+		*jt = Time(i)
+	}
+	return
+}
+
+// Value 获取driver.Value
+func (jt Time) Value() driver.Value {
+	return time.Unix(int64(jt), 0)
+}
+
+// Time 转化time.Time
+func (jt Time) Time() time.Time {
+	return time.Unix(int64(jt), 0)
+}
---
+++ timeout/ctime_test.go
@@ -0,0 +1,38 @@
+/******
+** @创建时间 : 2020/12/28 11:50
+** @作者 : SongZhiBin
+******/
+package ctime
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func TestShrink(t *testing.T) {
+	c1, cancel := context.WithTimeout(context.Background(), 5)
+	defer cancel()
+	type args struct {
+		c context.Context
+		d time.Duration
+	}
+	tests := []struct {
+		name string
+		args args
+		want time.Duration
+	}{
+		{"上游没有设置链路超时时间", args{c: context.Background(), d: 10}, 10},
+
+		{"上游链路设置链路时间且当前时间超时时间大于流转链路时间", args{c:c1, d: 10}, 5},
+		{"上路链路设置超时时间事件小于当前时间", args{c:c1, d: 3}, 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, _, _ := Shrink(tt.args.c, tt.args.d)
+			if got > tt.want {
+				t.Errorf("Shrink() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
