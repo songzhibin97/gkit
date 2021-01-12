2ee6f7e91eec1466a6bdab9308c03dfea8485dd7
songzhibin97 <718428482@qq.com>
2021-01-12 14:57:34 +0800

    修改用例

--- internal/window/init_test.go
+++ internal/window/init_test.go
@@ -7,12 +7,36 @@ package window
 import (
 	"strconv"
 	"testing"
+	"time"
 )
 
 func TestWindow(t *testing.T) {
 	w := InitWindow()
-	for i := (uint)(1); i < 10000000; i++ {
-		w.AddIndex(strconv.Itoa(int(i)),i)
+	slice := []Index{
+		{Name: "1", Score: 1}, {Name: "2", Score: 2},
+		{Name: "2", Score: 2}, {Name: "3", Score: 3},
+		{Name: "2", Score: 2}, {Name: "3", Score: 3},
+		{Name: "4", Score: 4}, {Name: "3", Score: 3},
+		{Name: "5", Score: 5}, {Name: "2", Score: 2},
+		{Name: "6", Score: 6}, {Name: "5", Score: 5},
+	}
+	for i := 0; i < len(slice); i += 2 {
+		w.AddIndex(slice[i].Name, slice[i].Score)
+		w.AddIndex(slice[i+1].Name, slice[i+1].Score)
+		time.Sleep(time.Second)
 		t.Log(w.Show())
 	}
+}
+
+func BenchmarkWindow(b *testing.B) {
+	w := InitWindow()
+	go func() {
+		for {
+			w.Show()
+		}
+	}()
+	for i := 0; i < b.N; i++ {
+		w.AddIndex(strconv.Itoa(i), uint(i))
+	}
+
 }
