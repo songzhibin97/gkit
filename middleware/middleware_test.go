e86c74077e7758226d7a0e614794bcc220c5650d
songzhibin97 <718428482@qq.com>
2021-01-21 17:11:57 +0800

    Create middleware_test.go

---
+++ middleware/middleware_test.go
@@ -0,0 +1,60 @@
+package middleware
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+var (
+	ctx = context.Background()
+	req = struct{}{}
+)
+
+func TestChain(t *testing.T) {
+	e := Chain(
+		annotate("first"),
+		annotate("second"),
+		annotate("third"),
+	)(myEndpoint)
+
+	if _, err := e(ctx, req); err != nil {
+		panic(err)
+	}
+	// Output:
+	// first pre
+	// second pre
+	// third pre
+	// my endpoint!
+	// third post
+	// second post
+	// first post
+}
+
+func ExampleChain() {
+	e := Chain(
+		annotate("first"),
+		annotate("second"),
+		annotate("third"),
+	)(myEndpoint)
+
+	if _, err := e(ctx, req); err != nil {
+		panic(err)
+	}
+}
+
+func annotate(s string) MiddleWare {
+	return func(next Endpoint) Endpoint {
+		return func(ctx context.Context, request interface{}) (interface{}, error) {
+
+			fmt.Println(s, "pre")
+			defer fmt.Println(s, "post")
+			return next(ctx, request)
+		}
+	}
+}
+
+func myEndpoint(context.Context, interface{}) (interface{}, error) {
+	fmt.Println("my endpoint!")
+	return struct{}{}, nil
+}
