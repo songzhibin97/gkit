c1f48e70e570adaad008a7bdcf913cce450dac9d
songzhibin97 <718428482@qq.com>
2021-01-21 16:53:24 +0800

    Update middleware.go

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,4 +1,23 @@
 package middleware
 
+import "context"
+
 // package middleware: 封装中间件格式
 
+// Endpoint:
+// func(ctx context.Context,request interface{}) (response interface{},err error)
+type Endpoint func(context.Context, interface{}) (interface{}, error)
+
+// MiddleWare: 方便链式操作
+type MiddleWare func(Endpoint) Endpoint
+
+// Chain: 连接成链路
+// outer: 最外层的
+func Chain(outer MiddleWare, others ...MiddleWare) MiddleWare {
+	return func(next Endpoint) Endpoint {
+		for i := len(others) - 1; i >= 0; i-- { // reverse
+			next = others[i](next)
+		}
+		return outer(next)
+	}
+}
