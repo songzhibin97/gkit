96d521150cc88c49781c59db1abcb4c3828ec0f0
songzhibin97 <718428482@qq.com>
2021-01-24 08:27:09 +0800

    增加了降级适配

--- downgrade/downgrade.go
+++ downgrade/downgrade.go
@@ -1,3 +1,31 @@
 package downgrade
 
+import (
+	"context"
+	"github.com/afex/hystrix-go/hystrix"
+)
+
 // package downgrade: 熔断降级
+// 与 "github.com/afex/hystrix-go/hystrix" 使用方法一致,只是做了抽象封装,避免因为升级对服务造成影响"
+
+type runFunc = func() error
+type fallbackFunc = func(error) error
+type runFuncC = func(context.Context) error
+type fallbackFuncC = func(context.Context, error) error
+
+// Fuse: 熔断降级接口
+type Fuse interface {
+	// Do: 以同步的方式运行 runFunc,直到成功为止
+	// 如果返回错误,执行 fallbackFunc 函数
+	Do(name string, run runFunc, fallback fallbackFunc) error
+
+	// Go: 异步调用返回 channel
+	Go(name string, run runFunc, fallback fallbackFunc) chan error
+
+	// GoC:
+	// Do/Go 都调用GoC, Do中处理了异步过程
+	GoC(ctx context.Context, name string, run runFuncC, fallback fallbackFuncC) chan error
+
+	// ConfigureCommand: 配置参数
+	ConfigureCommand(name string, config hystrix.CommandConfig)
+}
---
+++ downgrade/hystrix.go
@@ -0,0 +1,29 @@
+package downgrade
+
+import (
+	"context"
+	"github.com/afex/hystrix-go/hystrix"
+)
+
+type Hystrix struct{}
+
+func (h *Hystrix) Do(name string, run runFunc, fallback fallbackFunc) error {
+	return hystrix.Do(name, run, fallback)
+}
+
+func (h *Hystrix) Go(name string, run runFunc, fallback fallbackFunc) chan error {
+	return hystrix.Go(name, run, fallback)
+}
+
+func (h *Hystrix) GoC(ctx context.Context, name string, run runFuncC, fallback fallbackFuncC) chan error {
+	return hystrix.GoC(ctx, name, run, fallback)
+}
+
+func (h *Hystrix) ConfigureCommand(name string, config hystrix.CommandConfig) {
+	hystrix.ConfigureCommand(name, config)
+}
+
+// NewFuse: 实例化方法
+func NewFuse() Fuse {
+	return &Hystrix{}
+}
