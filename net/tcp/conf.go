7d1e076ebce003cada7a1f54990206251eb8900f
songzhibin97 <718428482@qq.com>
2021-03-13 19:04:50 +0800

    net封装

---
+++ net/tcp/conf.go
@@ -0,0 +1,28 @@
+package tcp
+
+import "time"
+
+var (
+	// DefaultWaitTimeout: 默认等待超时时间
+	DefaultWaitTimeout = time.Millisecond
+
+	// DefaultConnTimeout: 默认连接超时时间
+	DefaultConnTimeout = 30 * time.Second
+
+	// DefaultRetryInterval: 默认重试间隔
+	DefaultRetryInterval = 100 * time.Millisecond
+
+	// DefaultReadBuffer: 默认读取buffer大小
+	DefaultReadBuffer = 1 << 12
+
+	// DefaultServer: 默认的服务名称
+	DefaultServer = "Default"
+)
+
+const (
+	// Status
+
+	UNKNOWN = iota
+	ACTIVE
+	ERROR
+)
---
+++ net/tcp/conn_func.go
@@ -0,0 +1,43 @@
+package tcp
+
+import "time"
+
+// Send: 拨号并发送消息,关闭链接
+func Send(addr string, data []byte, retry *Retry) error {
+	c, err := NewConn(addr, nil)
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+	return c.Send(data, retry)
+}
+
+// SendRecv 拨号发送并读取响应
+func SendRecv(addr string, data []byte, length int, retry *Retry) ([]byte, error) {
+	conn, err := NewConn(addr, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+	return conn.SendRecv(data, length, retry)
+}
+
+// SendWithTimeout: 创建并发送具有写入超时限制的连接
+func SendWithTimeout(addr string, data []byte, timeout time.Duration, retry *Retry) error {
+	conn, err := NewConn(addr, nil)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	return conn.SendWithTimeout(data, timeout, retry)
+}
+
+// SendRecvWithTimeout: 创建链接发送读取有超时限制的连接
+func SendRecvWithTimeout(addr string, data []byte, receive int, timeout time.Duration, retry *Retry) ([]byte, error) {
+	conn, err := NewConn(addr, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+	return conn.SendRecvWithTimeout(data, timeout, receive, retry)
+}
