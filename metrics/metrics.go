4f3061445221257841269ce7a98011b9dd752643
songzhibin97 <718428482@qq.com>
2021-03-17 10:39:56 +0800

    Create metrics.go

---
+++ metrics/metrics.go
@@ -0,0 +1,25 @@
+package metrics
+
+// package metrics: 指标信息
+
+
+// Counter is metrics counter.
+type Counter interface {
+	With(lvs ...string) Counter
+	Inc()
+	Add(delta float64)
+}
+
+// Gauge is metrics gauge.
+type Gauge interface {
+	With(lvs ...string) Gauge
+	Set(value float64)
+	Add(delta float64)
+	Sub(delta float64)
+}
+
+// Observer is metrics observer.
+type Observer interface {
+	With(lvs ...string) Observer
+	Observe(float64)
+}
