8fb38ead5e79a68c38c48f312280fac2060cfd99
songzhibin97 <718428482@qq.com>
2021-06-05 08:36:21 +0800

    errors 重构

--- errors/error_test.go
+++ errors/error_test.go
@@ -3,71 +3,79 @@ package errors
 import (
 	"errors"
 	"fmt"
-	"github.com/golang/protobuf/proto"
-	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"net/http"
 	"testing"
 )
 
-func TestIsError(t *testing.T) {
-	s1 := &ErrorCode{Code: 1}
-	s2 := &ErrorCode{Code: 2}
-	if errors.Is(s1, s2) {
-		t.Errorf("error is not equal: %+v -> %+v", s1, s2)
-	}
-	s1.Code = 1
-	s2.Code = 1
-	if !errors.Is(s1, s2) {
-		t.Errorf("error is not equal: %+v -> %+v", s1, s2)
-	}
+func TestError(t *testing.T) {
+	var (
+		base *Error
+	)
+	err := Errorf(http.StatusBadRequest, "reason", "message")
+	err2 := Errorf(http.StatusBadRequest, "reason", "message")
+	err3 := err.AddMetadata(map[string]string{
+		"foo": "bar",
+	})
+	werr := fmt.Errorf("wrap %w", err)
 
-	s1.Reason = "test"
-	s2.Reason = "test"
-
-	if !errors.Is(s1, s2) {
-		t.Errorf("error is not equal: %+v -> %+v", s1, s2)
+	if errors.Is(err, new(Error)) {
+		t.Errorf("should not be equal: %v", err)
 	}
-
-	if ErrToReason(s1) != "test" {
-		t.Errorf("error is not equal: %+v", s1)
+	if !errors.Is(werr, err) {
+		t.Errorf("should be equal: %v", err)
+	}
+	if !errors.Is(werr, err2) {
+		t.Errorf("should be equal: %v", err)
 	}
 
-	if ErrToReason(s2) != "test" {
-		t.Errorf("error is not equal: %+v", s1)
+	if !errors.As(err, &base) {
+		t.Errorf("should be matchs: %v", err)
+	}
+	if !IsBadRequest(err) {
+		t.Errorf("should be matchs: %v", err)
 	}
 
-	err1 := &ErrorCode{Code: 1}
-	err2 := fmt.Errorf("warp err %w", err1)
-	if !errors.Is(err2, err1) {
-		t.Errorf("error is not equal: a: %v b: %v ", err2, err1)
+	if reason := Reason(err); reason != err3.Reason {
+		t.Errorf("got %s want: %s", reason, err)
+	}
+
+	if err3.Metadata["foo"] != "bar" {
+		t.Error("not expected metadata")
+	}
+
+	gs := err.GRPCStatus()
+	se := FromError(gs.Err())
+	if se.Reason != se.Reason {
+		t.Errorf("got %+v want %+v", se, err)
 	}
 }
 
-func TestErrorAs(t *testing.T) {
-	err1 := &ErrorCode{Code: 1}
-	err2 := fmt.Errorf("wrap : %w", err1)
 
-	err3 := new(ErrorCode)
-	if !errors.As(err2, &err3) {
-		t.Errorf("error is not equal: %v", err2)
-	}
-}
+func TestTypes(t *testing.T) {
+	var (
+		input = []error{
+			BadRequest("reason_400", "message_400"),
+			Unauthorized("reason_401", "message_401"),
+			Forbidden("reason_403", "message_403"),
+			NotFound("reason_404", "message_404"),
+			Conflict("reason_409", "message_409"),
+			InternalServer("reason_500", "message_500"),
+			ServiceUnavailable("reason_503", "message_503"),
+		}
+		output = []func(error) bool{
+			IsBadRequest,
+			IsUnauthorized,
+			IsForbidden,
+			IsNotFound,
+			IsConflict,
+			IsInternalServer,
+			IsServiceUnavailable,
+		}
+	)
 
-func TestAddDetails(t *testing.T) {
-	details := []proto.Message{
-		&errdetails.ErrorInfo{
-			Reason:   "reason",
-			Metadata: map[string]string{"message": "message"},
-		},
+	for i, in := range input {
+		if !output[i](in) {
+			t.Errorf("not expect: %v", in)
+		}
 	}
-	err1 := &ErrorCode{Code: 0}
-	err := err1.AddDetails(details...)
-	if !errors.Is(err, ErrDetails) {
-		t.Errorf("error is not equal: a: %v b: %v ", err, ErrDetails)
-	}
-	err2 := &ErrorCode{Code: 1}
-	err = err2.AddDetails(details...)
-	if err != nil {
-		t.Errorf("error is not nil %v", err)
-	}
-	t.Log(err2)
 }
--- go.mod
+++ go.mod
@@ -6,9 +6,9 @@ require (
 	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
 	github.com/afex/hystrix-go v0.0.0-20180502004556-fa1af6a1f4f5
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/emicklei/proto v1.9.0 // indirect
+	github.com/emicklei/proto v1.9.0
 	github.com/go-ole/go-ole v1.2.4 // indirect
-	github.com/golang/protobuf v1.4.3
+	github.com/golang/protobuf v1.5.2
 	github.com/juju/ratelimit v1.0.1
 	github.com/pkg/errors v0.9.1
 	github.com/shirou/gopsutil v3.20.11+incompatible
@@ -19,5 +19,6 @@ require (
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013
 	google.golang.org/grpc v1.35.0
-	gopkg.in/yaml.v2 v2.2.8 // indirect
+	google.golang.org/protobuf v1.26.0
+	gopkg.in/yaml.v2 v2.2.8
 )
