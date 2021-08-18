13a6f2d161ab4259cd0f69620703fd8ae3411013
songzhibin97 <718428482@qq.com>
2021-08-18 14:17:02 +0800

    feat:无所队列

---
+++ internal/sys/queue/queue.go
@@ -0,0 +1,78 @@
+package queue
+
+import (
+	"sync/atomic"
+	"unsafe"
+)
+
+// LKQueue lock-free的queue
+type LKQueue struct {
+	head unsafe.Pointer
+	tail unsafe.Pointer
+}
+
+// 通过链表实现，这个数据结构代表链表中的节点
+type node struct {
+	value interface{}
+	next  unsafe.Pointer
+}
+
+func NewLKQueue() *LKQueue {
+	n := unsafe.Pointer(&node{})
+	return &LKQueue{head: n, tail: n}
+}
+
+// Enqueue 入队
+func (q *LKQueue) Enqueue(v interface{}) {
+	n := &node{value: v}
+	for {
+		tail := load(&q.tail)
+		next := load(&tail.next)
+		if tail == load(&q.tail) { // 尾还是尾
+			if next == nil { // 还没有新数据入队
+				if cas(&tail.next, next, n) { //增加到队尾
+					cas(&q.tail, tail, n) //入队成功，移动尾巴指针
+					return
+				}
+			} else { // 已有新数据加到队列后面，需要移动尾指针
+				cas(&q.tail, tail, next)
+			}
+		}
+	}
+}
+
+// Dequeue 出队，没有元素则返回nil
+func (q *LKQueue) Dequeue() interface{} {
+	for {
+		head := load(&q.head)
+		tail := load(&q.tail)
+		next := load(&head.next)
+		if head == load(&q.head) { // head还是那个head
+			if head == tail { // head和tail一样
+				if next == nil { // 说明是空队列
+					return nil
+				}
+				// 只是尾指针还没有调整，尝试调整它指向下一个
+				cas(&q.tail, tail, next)
+			} else {
+				// 读取出队的数据
+				v := next.value
+				// 既然要出队了，头指针移动到下一个
+				if cas(&q.head, head, next) {
+					return v // Dequeue is done.  return
+				}
+			}
+		}
+	}
+}
+
+// 将unsafe.Pointer原子加载转换成node
+func load(p *unsafe.Pointer) (n *node) {
+	return (*node)(atomic.LoadPointer(p))
+}
+
+// 封装CAS,避免直接将*node转换成unsafe.Pointer
+func cas(p *unsafe.Pointer, old, new *node) (ok bool) {
+	return atomic.CompareAndSwapPointer(
+		p, unsafe.Pointer(old), unsafe.Pointer(new))
+}
