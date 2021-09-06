2f24b3689cfb6ee2e75d4975303d20a64bd81a79
songzhibin97 <718428482@qq.com>
2021-09-06 10:23:17 +0800

    style:更改命名方式

--- timeout/data_struct.go
+++ timeout/data_struct.go
@@ -7,13 +7,13 @@ import (
 	"time"
 )
 
-type MyDate struct {
+type DateStruct struct {
 	time.Time
 }
 
 // UnmarshalText 通过 string 序列化成 Date
 // Author [SliverHorn](https://github.com/SliverHorn)
-func (m *MyDate) UnmarshalText(value string) error {
+func (m *DateStruct) UnmarshalText(value string) error {
 	dd, err := time.Parse(DateFormat, value)
 	if err != nil {
 		return err
@@ -24,7 +24,7 @@ func (m *MyDate) UnmarshalText(value string) error {
 
 // UnmarshalTextByLayout 通过 string 序列化成 Date by layout
 // Author [SliverHorn](https://github.com/SliverHorn)
-func (m *MyDate) UnmarshalTextByLayout(layout, value string) error {
+func (m *DateStruct) UnmarshalTextByLayout(layout, value string) error {
 	dd, err := time.Parse(layout, value)
 	if err != nil {
 		return err
@@ -35,31 +35,31 @@ func (m *MyDate) UnmarshalTextByLayout(layout, value string) error {
 
 // ToDateTime Date to Datetime
 // Author [SliverHorn](https://github.com/SliverHorn)
-func (m *MyDate) ToDateTime() *MyDateTime {
-	return &MyDateTime{Time: m.Time}
+func (m *DateStruct) ToDateTime() *DateTimeStruct {
+	return &DateTimeStruct{Time: m.Time}
 }
 
 // UnmarshalJSON 反序列化
 // Author [SliverHorn](https://github.com/SliverHorn)
-func (m *MyDate) UnmarshalJSON(src []byte) error {
+func (m *DateStruct) UnmarshalJSON(src []byte) error {
 	return m.UnmarshalText(strings.Replace(string(src), "\"", "", -1))
 }
 
 // MarshalJSON 序列化
 // Author [SliverHorn](https://github.com/SliverHorn)
-func (m *MyDate) MarshalJSON() ([]byte, error) {
+func (m *DateStruct) MarshalJSON() ([]byte, error) {
 	return []byte(`"` + m.String() + `"`), nil
 }
 
 // String 输出 DateTime 变量为字符串
 // Author [SliverHorn](https://github.com/SliverHorn)
-func (m *MyDate) String() string {
+func (m *DateStruct) String() string {
 	return m.Format(DateFormat)
 }
 
 // Scan 扫描
 // Author [SliverHorn](https://github.com/SliverHorn)
-func (m *MyDate) Scan(value interface{}) error {
+func (m *DateStruct) Scan(value interface{}) error {
 	switch v := value.(type) {
 	case []byte:
 		return m.UnmarshalText(string(v))
@@ -68,7 +68,7 @@ func (m *MyDate) Scan(value interface{}) error {
 	case time.Time:
 		m.Time = v
 	case nil:
-		m = &MyDate{}
+		m = &DateStruct{}
 	default:
 		return fmt.Errorf("cannot sql.Scan() DBDate from: %#v", v)
 	}
@@ -77,12 +77,12 @@ func (m *MyDate) Scan(value interface{}) error {
 
 // Value 值
 // Author [SliverHorn](https://github.com/SliverHorn)
-func (m *MyDate) Value() (driver.Value, error) {
+func (m *DateStruct) Value() (driver.Value, error) {
 	return driver.Value(m.Format(DateFormat)), nil
 }
 
 // GormDataType gorm 定义数据库字段类型
 // Author [SliverHorn](https://github.com/SliverHorn)
-func (m *MyDate) GormDataType() string {
+func (m *DateStruct) GormDataType() string {
 	return "DATE"
 }
