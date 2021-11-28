d1dbbfb8d5ab4c22f2b6801e7dd512a20f4653c9
songzhibin97 <718428482@qq.com>
2021-11-28 14:28:07 +0800

    feat:扇入模式

---
+++ concurrent/fan_in.go
@@ -0,0 +1,53 @@
+package concurrent
+
+import "reflect"
+
+// FanInRec 扇入模式
+func FanInRec(channels ...<-chan interface{}) <-chan interface{} {
+	out := make(chan interface{},1)
+	go func() {
+		defer close(out)
+		var cases []reflect.SelectCase
+		for _, channel := range channels {
+			cases = append(cases, reflect.SelectCase{
+				Dir:  reflect.SelectRecv,
+				Chan: reflect.ValueOf(channel),
+			})
+		}
+		for len(cases) > 0 {
+			i, v, ok := reflect.Select(cases)
+			if !ok {
+				// 监控的channel已经关闭
+				cases = append(cases[:i], cases[i+1:]...)
+				continue
+			}
+			out <- v.Interface()
+		}
+	}()
+	return out
+}
+
+// MergeChannel 合并两个channel
+func MergeChannel(a, b <-chan interface{}) <-chan interface{} {
+	c := make(chan interface{})
+	go func() {
+		defer close(c)
+		for a != nil || b != nil {
+			select {
+			case v, ok := <-a:
+				if !ok {
+					a = nil
+					continue
+				}
+				c <- v
+			case v, ok := <-b:
+				if !ok {
+					b = nil
+					continue
+				}
+				c <- v
+			}
+		}
+	}()
+	return c
+}
---
+++ concurrent/fan_in_test.go
@@ -0,0 +1,45 @@
+package concurrent
+
+import (
+	"github.com/stretchr/testify/assert"
+	"sort"
+	"testing"
+)
+
+func fanIn(start, end int) <-chan interface{} {
+	out := make(chan interface{})
+	go func() {
+		defer close(out)
+		for i := start; i < end; i++ {
+			out <- i
+		}
+	}()
+	return out
+}
+
+func TestFanInRec(t *testing.T) {
+	out := FanInRec(fanIn(0, 6), fanIn(6, 11), fanIn(11, 20))
+	outSlice := make([]interface{}, 0)
+	for v := range out {
+		outSlice = append(outSlice, v)
+	}
+	assert.Len(t, outSlice, 20)
+	sort.Slice(outSlice, func(i, j int) bool {
+		return outSlice[i].(int) < outSlice[j].(int)
+	})
+	assert.Equal(t, outSlice, []interface{}{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19})
+
+}
+
+func TestMergeChannel(t *testing.T) {
+	out := MergeChannel(fanIn(0, 6), fanIn(6, 11))
+	outSlice := make([]interface{}, 0)
+	for v := range out {
+		outSlice = append(outSlice, v)
+	}
+	assert.Len(t, outSlice, 10)
+	sort.Slice(outSlice, func(i, j int) bool {
+		return outSlice[i].(int) < outSlice[j].(int)
+	})
+	assert.Equal(t, outSlice, []interface{}{0,1,2,3,4,5,6,7,8,9,10})
+}
