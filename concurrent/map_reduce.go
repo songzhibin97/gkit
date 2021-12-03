3c9eefcf538f163cdf0ee2c28dd83c7d4ec117e3
songzhibin97 <718428482@qq.com>
2021-12-03 10:43:39 +0800

    feat:map_reduce

---
+++ concurrent/map_reduce.go
@@ -0,0 +1,27 @@
+package concurrent
+
+func MapChan(in <-chan interface{}, fn func(interface{}) interface{}) <-chan interface{} {
+	out := make(chan interface{},1)
+	if in == nil {
+		close(out)
+		return out
+	}
+	go func() {
+		defer close(out)
+		for v := range in {
+			out <- fn(v)
+		}
+	}()
+	return out
+}
+
+func ReduceChan(in <-chan interface{}, fn func(r, v interface{}) interface{}) interface{} {
+	if in == nil {
+		return nil
+	}
+	out := <-in
+	for v := range in {
+		out = fn(out, v)
+	}
+	return out
+}
