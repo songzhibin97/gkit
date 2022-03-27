5e2c2c7da96b6c799a1972a845278b15403e4645
songzhibin97 <718428482@qq.com>
2022-03-27 20:26:49 +0800

    feat:ggroup

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -51,8 +51,16 @@ func (g *Goroutine) _go() {
 		}()
 		defer atomic.AddInt64(&g.n, -1)
 		defer g.wait.Done()
+		t := time.NewTicker(g.checkTime)
+		defer t.Stop()
 		for {
 			select {
+			case <-t.C:
+				// 检查是否闲置
+				if atomic.LoadInt64(&g.idle) > atomic.LoadInt64(&g.n) {
+					// 闲置数超过预期
+					return
+				}
 			case f, ok := <-g.task:
 				// channel已经被关闭
 				if !ok {
@@ -64,6 +72,7 @@ func (g *Goroutine) _go() {
 					// 如果已经超出预定值,则该goroutine退出
 					return
 				}
+				t.Reset(g.checkTime)
 			case <-g.ctx.Done():
 				// 触发ctx退出
 				return
@@ -79,10 +88,16 @@ func (g *Goroutine) AddTask(f func()) bool {
 	if atomic.LoadInt32(&g.close) != 0 {
 		return false
 	}
-	if atomic.LoadInt64(&g.max) > atomic.LoadInt64(&g.n) {
-		g._go()
+	// 尝试直接塞入
+	// 如果阻塞尝试进行扩容
+	select {
+	case g.task <- f:
+	default:
+		if atomic.LoadInt64(&g.max) > atomic.LoadInt64(&g.n) {
+			g._go()
+		}
+		g.task <- f
 	}
-	g.task <- f
 	return true
 }
 
@@ -126,11 +141,12 @@ func (g *Goroutine) Shutdown() error {
 	return err
 }
 
-// trick Debug使用
-func (g *Goroutine) trick() {
+// Trick Debug使用
+func (g *Goroutine) Trick() string {
 	if g.logger != nil {
 		g.logger.Log(log.LevelDebug, atomic.LoadInt64(&g.max), atomic.LoadInt64(&g.n), len(g.task))
 	}
+	return fmt.Sprintln("max:", atomic.LoadInt64(&g.max), "idle:", atomic.LoadInt64(&g.idle), "now goroutine:", atomic.LoadInt64(&g.n), "task len:", len(g.task))
 }
 
 // Delegate 委托执行 一般用于回收函数超时控制
@@ -176,7 +192,8 @@ func NewGoroutine(ctx context.Context, opts ...options.Option) GGroup {
 	ctx, cancel := context.WithCancel(ctx)
 	o := config{
 		stopTimeout: 10 * time.Second,
-		max:         10,
+		max:         1000,
+		idle:        1000,
 	}
 	for _, opt := range opts {
 		opt(&o)
