c89626c4a7f617e7a55e2cbddcbb10b7e17094db
songzhibin97 <718428482@qq.com>
2022-07-25 18:36:24 +0800

    feat: rand_string

--- tools/bind/internal/bytesconv/bytesconv_test.go
+++ tools/bind/internal/bytesconv/bytesconv_test.go
@@ -6,10 +6,9 @@ package bytesconv
 
 import (
 	"bytes"
+	"github.com/songzhibin97/gkit/tools/rand_string"
 	"math/rand"
-	"strings"
 	"testing"
-	"time"
 )
 
 var (
@@ -37,37 +36,9 @@ func TestBytesToString(t *testing.T) {
 	}
 }
 
-const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const (
-	letterIdxBits = 6                    // 6 bits to represent a letter index
-	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
-	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
-)
-
-var src = rand.NewSource(time.Now().UnixNano())
-
-func RandStringBytesMaskImprSrcSB(n int) string {
-	sb := strings.Builder{}
-	sb.Grow(n)
-	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
-	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
-		if remain == 0 {
-			cache, remain = src.Int63(), letterIdxMax
-		}
-		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
-			sb.WriteByte(letterBytes[idx])
-			i--
-		}
-		cache >>= letterIdxBits
-		remain--
-	}
-
-	return sb.String()
-}
-
 func TestStringToBytes(t *testing.T) {
 	for i := 0; i < 100; i++ {
-		s := RandStringBytesMaskImprSrcSB(64)
+		s := rand_string.RandStringBytesMaskImprSrc(64)
 		if !bytes.Equal(rawStrToBytes(s), StringToBytes(s)) {
 			t.Fatal("don't match")
 		}
