21bc9408333e12b1b6ed3e979dd29acdef8f17c9
songzhibin97 <718428482@qq.com>
2022-12-16 17:01:11 +0800

    feat: stm

---
+++ tools/stm/stm.go
@@ -0,0 +1,59 @@
+package stm
+
+import (
+	"reflect"
+	"unsafe"
+)
+
+// struct => map
+
+func StructToMap(val interface{}, tag string) map[string]interface{} {
+	return structToMap(val, tag)
+}
+
+func structToMap(val interface{}, tag string) map[string]interface{} {
+	t, v := reflect.TypeOf(val), reflect.ValueOf(val)
+	if v.Kind() == reflect.Ptr {
+		return structToMap(v.Elem().Interface(), tag)
+	}
+	if v.Kind() != reflect.Struct {
+		return nil
+	}
+	mp := make(map[string]interface{})
+
+	for i := 0; i < v.NumField(); i++ {
+		vField := v.Field(i)
+		name := t.Field(i).Name
+		if tag != "" {
+			ts := t.Field(i).Tag.Get(tag)
+			if ts == "-" {
+				continue
+			}
+			if ts != "" {
+				name = ts
+			}
+		}
+		if !vField.IsValid() {
+			continue
+		}
+		st := (vField.Kind() == reflect.Struct) || (vField.Kind() == reflect.Ptr && vField.Elem().Kind() == reflect.Struct)
+		if vField.CanInterface() {
+			iv := vField.Interface()
+			if st {
+				iv = structToMap(iv, tag)
+			}
+			mp[name] = iv
+		} else {
+			// 未导出
+			cp := reflect.New(v.Type()).Elem()
+			cp.Set(v)
+			value := cp.Field(i)
+			iv := reflect.NewAt(value.Type(), unsafe.Pointer(value.UnsafeAddr())).Elem().Interface()
+			if st {
+				iv = structToMap(iv, tag)
+			}
+			mp[name] = iv
+		}
+	}
+	return mp
+}
---
+++ tools/stm/stm_test.go
@@ -0,0 +1,107 @@
+package stm
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type testS struct {
+	S1 string `stm:"S_1"  stm2:"-"`
+	s2 string `stm:"s_2"`
+	T1 struct {
+		Name string `stm:"name"`
+		age  int    `stm:"age" stm2:"-"`
+	} `stm:"T_1"`
+	t2 struct {
+		Gender bool `stm:"gender"`
+	} `stm2:"-"`
+	List1 []string `stm:"list1"  stm2:"-"`
+	list2 []int    `stm:"list2"`
+}
+
+func Test_structToMap(t *testing.T) {
+	ts := testS{
+		S1: "s1",
+		s2: "s2",
+		T1: struct {
+			Name string `stm:"name"`
+			age  int    `stm:"age" stm2:"-"`
+		}{
+			Name: "name",
+			age:  18,
+		},
+		t2: struct {
+			Gender bool `stm:"gender"`
+		}{
+			Gender: true,
+		},
+		List1: []string{"1", "2", "3"},
+		list2: []int{1, 2, 3},
+	}
+	type args struct {
+		tag string
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[string]interface{}
+	}{
+		{
+			name: "",
+			args: args{
+				tag: "",
+			},
+			want: map[string]interface{}{
+				"S1": "s1",
+				"s2": "s2",
+				"T1": map[string]interface{}{
+					"Name": "name",
+					"age":  18,
+				},
+				"t2": map[string]interface{}{
+					"Gender": true,
+				},
+				"List1": []string{"1", "2", "3"},
+				"list2": []int{1, 2, 3},
+			},
+		},
+		{
+			name: "",
+			args: args{
+				tag: "stm",
+			},
+			want: map[string]interface{}{
+				"S_1": "s1",
+				"s_2": "s2",
+				"T_1": map[string]interface{}{
+					"name": "name",
+					"age":  18,
+				},
+				"t2": map[string]interface{}{
+					"gender": true,
+				},
+				"list1": []string{"1", "2", "3"},
+				"list2": []int{1, 2, 3},
+			},
+		},
+		{
+			name: "",
+			args: args{
+				tag: "stm2",
+			},
+			want: map[string]interface{}{
+				"s2": "s2",
+				"T1": map[string]interface{}{
+					"Name": "name",
+				},
+				"list2": []int{1, 2, 3},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, structToMap(ts, tt.args.tag), tt.want)
+		})
+	}
+}
