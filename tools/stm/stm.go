ce65b278dbbc6a6f8e77bfc846cd731ee6f9f8f1
songzhibin97 <49082129+songzhibin97@users.noreply.github.com>
2022-12-19 14:18:55 +0800

    feat: stm extra export model

--- tools/stm/stm.go
+++ tools/stm/stm.go
@@ -8,13 +8,17 @@ import (
 // struct => map
 
 func StructToMap(val interface{}, tag string) map[string]interface{} {
-	return structToMap(val, tag)
+	return structToMap(val, tag, false)
 }
 
-func structToMap(val interface{}, tag string) map[string]interface{} {
+func StructToMapExtraExport(val interface{}, tag string) map[string]interface{} {
+	return structToMap(val, tag, true)
+}
+
+func structToMap(val interface{}, tag string, extraExport bool) map[string]interface{} {
 	t, v := reflect.TypeOf(val), reflect.ValueOf(val)
 	if v.Kind() == reflect.Ptr {
-		return structToMap(v.Elem().Interface(), tag)
+		return structToMap(v.Elem().Interface(), tag, extraExport)
 	}
 	if v.Kind() != reflect.Struct {
 		return nil
@@ -40,17 +44,17 @@ func structToMap(val interface{}, tag string) map[string]interface{} {
 		if vField.CanInterface() {
 			iv := vField.Interface()
 			if st {
-				iv = structToMap(iv, tag)
+				iv = structToMap(iv, tag, extraExport)
 			}
 			mp[name] = iv
-		} else {
+		} else if extraExport {
 			// 未导出
 			cp := reflect.New(v.Type()).Elem()
 			cp.Set(v)
 			value := cp.Field(i)
 			iv := reflect.NewAt(value.Type(), unsafe.Pointer(value.UnsafeAddr())).Elem().Interface()
 			if st {
-				iv = structToMap(iv, tag)
+				iv = structToMap(iv, tag, extraExport)
 			}
 			mp[name] = iv
 		}
--- tools/stm/stm_test.go
+++ tools/stm/stm_test.go
@@ -20,7 +20,7 @@ type testS struct {
 	list2 []int    `stm:"list2"`
 }
 
-func Test_structToMap(t *testing.T) {
+func Test_StructToMapExtraExport(t *testing.T) {
 	ts := testS{
 		S1: "s1",
 		s2: "s2",
@@ -101,7 +101,79 @@ func Test_structToMap(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, structToMap(ts, tt.args.tag), tt.want)
+			assert.Equal(t, StructToMapExtraExport(ts, tt.args.tag), tt.want)
+		})
+	}
+}
+
+func Test_StructToMap(t *testing.T) {
+	ts := testS{
+		S1: "s1",
+		s2: "s2",
+		T1: struct {
+			Name string `stm:"name"`
+			age  int    `stm:"age" stm2:"-"`
+		}{
+			Name: "name",
+			age:  18,
+		},
+		t2: struct {
+			Gender bool `stm:"gender"`
+		}{
+			Gender: true,
+		},
+		List1: []string{"1", "2", "3"},
+		list2: []int{1, 2, 3},
+	}
+	type args struct {
+		tag string
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[string]interface{}
+	}{
+		{
+			name: "",
+			args: args{
+				tag: "",
+			},
+			want: map[string]interface{}{
+				"S1": "s1",
+				"T1": map[string]interface{}{
+					"Name": "name",
+				},
+				"List1": []string{"1", "2", "3"},
+			},
+		},
+		{
+			name: "",
+			args: args{
+				tag: "stm",
+			},
+			want: map[string]interface{}{
+				"S_1": "s1",
+				"T_1": map[string]interface{}{
+					"name": "name",
+				},
+				"list1": []string{"1", "2", "3"},
+			},
+		},
+		{
+			name: "",
+			args: args{
+				tag: "stm2",
+			},
+			want: map[string]interface{}{
+				"T1": map[string]interface{}{
+					"Name": "name",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, StructToMap(ts, tt.args.tag), tt.want)
 		})
 	}
 }
