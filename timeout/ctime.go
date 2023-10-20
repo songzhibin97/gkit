ba2c6768228dda0e18b425edf5c6b66125c71d8c
songzhibin97 <718428482@qq.com>
2023-10-20 11:31:17 +0800

    Update ctime.go

--- timeout/ctime.go
+++ timeout/ctime.go
@@ -20,3 +20,20 @@ func Shrink(c context.Context, d time.Duration) (time.Duration, context.Context,
 	ctx, cancel := context.WithTimeout(c, d)
 	return d, ctx, cancel
 }
+
+// Compare 用于比较两个context的超时时间,返回超时时间最小的context
+func Compare(c1, c2 context.Context) context.Context {
+	c1Deadline, ok := c1.Deadline()
+	if !ok {
+		return c2
+	}
+	c2Deadline, ok := c2.Deadline()
+	if !ok {
+		return c1
+	}
+	if c1Deadline.Before(c2Deadline) {
+		return c1
+	} else {
+		return c2
+	}
+}
