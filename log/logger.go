0cb0fa364a9d6bf6f517dd4bf6c9b6ea1202593c
songzhibin97 <718428482@qq.com>
2026-04-29 22:47:50 +0800

    fix(log): call bindValues after appending kvs to resolve all Valuers
    bindValues was called before kvs were appended, so Valuers passed
    directly to Log() were never resolved. Moved append before bind.

--- log/logger.go
+++ log/logger.go
@@ -29,12 +29,9 @@ type logger struct {
 func (l *logger) Log(lever Lever, kvs ...interface{}) error {
 	nKvs := make([]interface{}, 0, len(l.prefix)+len(kvs))
 	nKvs = append(nKvs, l.prefix...)
-	if l.hasValuer && len(kvs) > 0 {
-		// 特殊处理
+	nKvs = append(nKvs, kvs...)
+	if l.hasValuer {
 		bindValues(l.ctx, nKvs)
-	}
-	if len(kvs) > 0 {
-		nKvs = append(nKvs, kvs...)
 	}
 
 	for _, log := range l.logs {
