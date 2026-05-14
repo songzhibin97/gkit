2db5e8ff8b07d7e2d931fd7e7e9560cf9bf801f3
Bin <49082129+songzhibin97@users.noreply.github.com>
2026-05-14 16:23:21 +0800

    fix(coding/proto): return error instead of panic on wrong type (#50)
    Marshal/Unmarshal used direct type assertion v.(proto.Message) which
    panics when the wrong type is passed. Changed to comma-ok assertion
    with a descriptive error message.

--- coding/proto/proto.go
+++ coding/proto/proto.go
@@ -1,6 +1,8 @@
 package proto
 
 import (
+	"fmt"
+
 	"github.com/songzhibin97/gkit/coding"
 	"google.golang.org/protobuf/proto"
 )
@@ -14,11 +16,19 @@ func init() {
 type code struct{}
 
 func (c code) Marshal(v interface{}) ([]byte, error) {
-	return proto.Marshal(v.(proto.Message))
+	m, ok := v.(proto.Message)
+	if !ok {
+		return nil, fmt.Errorf("proto: Marshal expects proto.Message, got %T", v)
+	}
+	return proto.Marshal(m)
 }
 
 func (c code) Unmarshal(data []byte, v interface{}) error {
-	return proto.Unmarshal(data, v.(proto.Message))
+	m, ok := v.(proto.Message)
+	if !ok {
+		return fmt.Errorf("proto: Unmarshal expects proto.Message, got %T", v)
+	}
+	return proto.Unmarshal(data, m)
 }
 
 func (c code) Name() string {
