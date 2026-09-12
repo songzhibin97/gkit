2d1b9ebeb16ff0bd9a0dd98454deb276c1ab8a08
songzhibin97 <718428482@qq.com>
2026-09-13 03:21:13 +0800

    test(distributed): isolate registration Redis fixture (#166)

--- distributed/service_test.go
+++ distributed/service_test.go
@@ -3,6 +3,9 @@ package distributed
 import (
 	"context"
 	"testing"
+	"time"
+
+	"github.com/alicebob/miniredis/v2"
 
 	"github.com/songzhibin97/gkit/log"
 
@@ -16,24 +19,39 @@ import (
 	"github.com/songzhibin97/gkit/distributed/locker/lock_redis"
 )
 
-func initServer() *Server {
+func initServer(t *testing.T) *Server {
+	t.Helper()
+	mr := miniredis.RunT(t)
 	opt := redis.UniversalOptions{
-		Addrs: []string{"127.0.0.1:6379"},
+		Addrs: []string{mr.Addr()},
 	}
 	client := redis.NewUniversalClient(&opt)
-	if client == nil {
-		return nil
-	}
+	t.Cleanup(func() {
+		if err := client.Close(); err != nil {
+			t.Error(err)
+		}
+	})
 	lock := lock_redis.NewRedisLock(client)
 	bk := broker.NewBroker(broker.NewRegisteredTask(), context.Background())
 	c := controller_redis.NewControllerRedis(bk, client, "test_task", "delayed")
 	backend := backend_redis.NewBackendRedis(client, -1)
-	return NewServer(c, backend, lock, log.NewHelper(log.With(log.DefaultLogger)), nil)
+	server, err := NewServerE(c, backend, lock, log.NewHelper(log.With(log.DefaultLogger)), nil)
+	t.Cleanup(func() {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		if err := server.Shutdown(ctx); err != nil {
+			t.Error(err)
+		}
+	})
+	if err != nil {
+		t.Fatalf("initialize isolated test server: %v", err)
+	}
+	return server
 }
 
 func TestRegisterTasks(t *testing.T) {
 	t.Parallel()
-	s := initServer()
+	s := initServer(t)
 	_, ok := s.GetRegisteredTask("test_task")
 	assert.False(t, ok)
 	err := s.RegisteredTasks(map[string]interface{}{
@@ -47,7 +65,7 @@ func TestRegisterTasks(t *testing.T) {
 
 func TestRegisterTask(t *testing.T) {
 	t.Parallel()
-	s := initServer()
+	s := initServer(t)
 	_, ok := s.GetRegisteredTask("test_task")
 	assert.False(t, ok)
 
